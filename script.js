4367072a3dfc1d8fcffccc534f17969d841b52e3
Anthony Tsui <68526943+s3791898@users.noreply.github.com>
2023-12-18 16:42:38 +1100

    Initial project setup
    Created the basic files required, and empty functions to get a general idea of what functions are required for this project.

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+
+}
+
+function subtract (a, b) {
+
+}
+
+function multiply (a, b) {
+
+}
+
+function divide (a, b) {
+  
+}
+
+function operate(operator, num1, num2, callback) {
+
+}
