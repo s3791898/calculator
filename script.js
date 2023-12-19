b1b5e6f90e1bb720a19e6e29f71bbdf65d5fa130
Anthony Tsui <68526943+s3791898@users.noreply.github.com>
2023-12-19 18:09:26 +1100

    Added all necessary html elements and styling
    I've created all the html elements and styled them with vibrant colours and included logic basic functions which include, add, subtract, multiply and divide.

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
+const display = document.getElementById("display");
+
 function add(a, b) {
-
+  return a + b;
 }
 
-function subtract (a, b) {
-
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply (a, b) {
-
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide (a, b) {
-  
+function divide(a, b) {
+  return a / b;
 }
 
-function operate(operator, num1, num2, callback) {
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+}
 
+function clearDisplay() {
+  display.value = "";
+}
+
+function appendToDisplay(input) {
+  display.value += input;
 }
