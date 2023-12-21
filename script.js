806d4e2c1d72ca58486fcb3207e22adf7897c027
Anthony Tsui <68526943+s3791898@users.noreply.github.com>
2023-12-21 12:34:41 +1100

    Initialised variables and added delete function
    Still working on the operate functions logic to make the calculator work.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+let firstOperand = "";
+let secondOperand = "";
+let operator = null;
+let displayValue = "";
+
 const display = document.getElementById("display");
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const clearButton = document.querySelector("[data-clear]");
 
 function add(a, b) {
   return a + b;
@@ -19,12 +28,38 @@ function divide(a, b) {
 function operate(operator, a, b) {
   a = Number(a);
   b = Number(b);
+
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      if (b === 0) {
+        return null;
+      } else {
+        return divide(a, b);
+      }
+    default:
+      return null;
+  }
 }
 
 function clearDisplay() {
   display.value = "";
+  firstOperand = "";
+  secondOperand = "";
+  operator = null;
 }
 
 function appendToDisplay(input) {
+  displayValue += input;
   display.value += input;
+}
+
+function clearLastEntry() {
+  displayValue = displayValue.slice(0, -1);
+  display.value = displayValue;
 }
