e7173786d83864f924958b3ce4f4a09c231c2a8f
Anthony Tsui <68526943+s3791898@users.noreply.github.com>
2023-12-22 15:13:38 +1100

    Calculator can now compute simple mathematical evaluations
    There are still some bugs that need to be addressed such as users being able to enter the arithmetic operator multiple times and the calculator isn't currently able to evaluate several operations at once

--- script.js
+++ script.js
@@ -26,52 +26,93 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, a, b) {
+// I need to figure out how to retrieve and store the first operand, second operand and the operator values in the display
+// Might be able to use the split method to retrieve the first operand, second operand and the operator
+function operate() {
+  // Get the current display value
+  const currentDisplayValue = display.value;
+  // Split the displayValue into an array of operands and operator
+  const operandsAndOperator = currentDisplayValue.split(/([+-/*])/);
+
+  // Extract the operands and operator
+  firstOperand = operandsAndOperator[0];
+  operator = operandsAndOperator[1];
+  secondOperand = operandsAndOperator[2];
+
+  // Check if all necessary values are present
+  if (firstOperand && operator && secondOperand) {
+    if (operator === "/" && parseFloat(secondOperand) === 0) {
+      // Division by zero error
+      display.value = "Undefined";
+    } else {
+      // Perform the calculation
+      result = calculate(
+        parseFloat(firstOperand),
+        operator,
+        parseFloat(secondOperand)
+      );
+
+      // Display the result
+      display.value = result;
+      // Update the displayValue for further calculations
+      displayValue = result.toString();
+    }
+  }
+}
+
+function calculate(a, operator, b) {
   a = Number(a);
   b = Number(b);
-  let result = "";
-
-  // I need to figure out how to retrieve and store the first operand, second operand and the operator values in the display
-  // Might be able to use the split method to retrieve the first operand, second operand and the operator
-
   switch (operator) {
     case "+":
-      result = add(a, b);
-      break;
+      return add(a, b);
     case "-":
-      result = subtract(a, b);
-      break;
+      return subtract(a, b);
     case "*":
-      result = multiply(a, b);
-      break;
+      return multiply(a, b);
     case "/":
-      if (b === 0) {
-        result = null;
-      } else {
-        result = divide(a, b);
-      }
-      break;
+      return b === 0 ? null : divide(a, b);
     default:
-      result = null;
+      return null;
   }
-
-  display.value = result;
-  return result;
 }
 
 function clearDisplay() {
   display.value = "";
   firstOperand = "";
+  1231;
   secondOperand = "";
   operator = null;
+  displayValue = "";
 }
 
 function appendToDisplay(input) {
+  // Check if the input is an opeator and if there is already an operator
   displayValue += input;
   display.value += input;
 }
 
-function clearLastEntry() {
+function deleteLastEntry() {
   displayValue = displayValue.slice(0, -1);
   display.value = displayValue;
+}
+
+document.addEventListener("keydown", handleKeyPress);
+
+function handleKeyPress(event) {
+  const key = event.key;
+
+  if (key >= "0" && key <= "9") {
+    appendToDisplay(key);
+  } else if (key === "." && displayValue.indexOf(".") === -1) {
+    appendToDisplay(key);
+  } else if (key === "+" || key === "-" || key === "*" || key === "/") {
+    appendToDisplay(key);
+  } else if (key === "Enter") {
+    operate();
+  } else if (key === "Delete") {
+    clearDisplay();
+  } else if (key === "Backspace") {
+    deleteLastEntry();
+  }
 }
