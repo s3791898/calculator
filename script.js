7f9309c9690acb09c9437c75fc4698028ce3cc46
Anthony Tsui <68526943+s3791898@users.noreply.github.com>
2023-12-25 21:01:27 +1100

    Changed the display layout and added decimal functionality
    Users can now perform calculations accurately. However there is still some bugs with keyboard support that needs to be fixed.

--- script.js
+++ script.js
@@ -1,14 +1,67 @@
-let firstOperand = "";
-let secondOperand = "";
-let operator = null;
-let displayValue = "";
+let previousValue = "";
+let currentValue = "";
+let operator = "";
 
-const display = document.getElementById("display");
-const numberButtons = document.querySelectorAll("[data-number]");
-const operationButtons = document.querySelectorAll("[data-operation]");
-const equalsButton = document.querySelector("[data-equals]");
-const clearButton = document.querySelector("[data-clear]");
-const deleteButton = document.querySelector("[data-delete]");
+// Store all components on HTML in our JS
+let clear = document.querySelector("#clear-btn");
+let backspace = document.querySelector("#delete-btn");
+let decimal = document.querySelector(".decimal");
+let equal = document.querySelector(".equal");
+
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+
+let previousScreen = document.querySelector(".previous");
+let currentScreen = document.querySelector(".current");
+
+numbers.forEach((number) =>
+  number.addEventListener("click", function (e) {
+    handleNumber(e.target.textContent);
+    currentScreen.textContent = currentValue;
+  })
+);
+
+operators.forEach((op) =>
+  op.addEventListener("click", function (e) {
+    handleOperator(e.target.textContent);
+    previousScreen.textContent = previousValue + " " + operator;
+    currentScreen.textContent = currentValue;
+  })
+);
+
+// Logic and event listener for clear button
+clear.addEventListener("click", clearScreen);
+
+// Logic and event listener for delete button
+backspace.addEventListener("click", deleteLastEntry);
+
+// Logic and event listener for equal button
+equal.addEventListener("click", operate);
+
+// Logic and event listener for clear decimal button
+decimal.addEventListener("click", addDecimal);
+
+function handleNumber(num) {
+  if (currentValue.length <= 7) {
+    currentValue += num;
+    currentScreen.textContent = currentValue;
+  }
+}
+
+function handleOperator(op) {
+  operator = op;
+  previousValue = currentValue;
+  currentValue = "";
+  previousScreen.textContent = previousValue + " " + operator;
+  currentScreen.textContent = currentValue;
+}
+
+function addDecimal() {
+  if (!currentValue.includes(".")) {
+    currentValue += ".";
+    currentScreen.textContent = currentValue;
+  }
+}
 
 function add(a, b) {
   return a + b;
@@ -26,36 +79,29 @@ function divide(a, b) {
   return a / b;
 }
 
-// I need to figure out how to retrieve and store the first operand, second operand and the operator values in the display
-// Might be able to use the split method to retrieve the first operand, second operand and the operator
 function operate() {
-  // Get the current display value
-  const currentDisplayValue = display.value;
-  // Split the displayValue into an array of operands and operator
-  const operandsAndOperator = currentDisplayValue.split(/([+-/*])/);
+  if (currentValue != "" && previousValue != "") {
+    // Call calculate and store the result
+    let result = calculate(previousValue, operator, currentValue);
 
-  // Extract the operands and operator
-  firstOperand = operandsAndOperator[0];
-  operator = operandsAndOperator[1];
-  secondOperand = operandsAndOperator[2];
-
-  // Check if all necessary values are present
-  if (firstOperand && operator && secondOperand) {
-    if (operator === "/" && parseFloat(secondOperand) === 0) {
-      // Division by zero error
-      display.value = "Undefined";
+    // Check if the result is not null
+    if (result !== null) {
+      // Update screens and variables with the result
+      previousScreen.textContent = "";
+      currentScreen.textContent = isWholeNumber(result)
+        ? result.toFixed(0)
+        : result.toFixed(1);
+      previousValue = isWholeNumber(result)
+        ? result.toFixed(0)
+        : result.toFixed(1);
+      currentValue = previousValue;
+      operator = "";
     } else {
-      // Perform the calculation
-      result = calculate(
-        parseFloat(firstOperand),
-        operator,
-        parseFloat(secondOperand)
-      );
-
-      // Display the result
-      display.value = result;
-      // Update the displayValue for further calculations
-      displayValue = result.toString();
+      // Handle division by zero error
+      currentScreen.textContent = "Undefined";
+      previousValue = "";
+      currentValue = "";
+      operator = "";
     }
   }
 }
@@ -63,12 +109,13 @@ function operate() {
 function calculate(a, operator, b) {
   a = Number(a);
   b = Number(b);
+
   switch (operator) {
     case "+":
       return add(a, b);
     case "-":
       return subtract(a, b);
-    case "*":
+    case "x":
       return multiply(a, b);
     case "/":
       return b === 0 ? null : divide(a, b);
@@ -77,24 +124,22 @@ function calculate(a, operator, b) {
   }
 }
 
-function clearDisplay() {
-  display.value = "";
-  firstOperand = "";
-  1231;
-  secondOperand = "";
-  operator = null;
-  displayValue = "";
-}
-
-function appendToDisplay(input) {
-  // Check if the input is an opeator and if there is already an operator
-  displayValue += input;
-  display.value += input;
+function clearScreen() {
+  previousValue = "";
+  currentValue = "";
+  operator = "";
+  previousScreen.textContent = "";
+  currentScreen.textContent = "";
 }
 
 function deleteLastEntry() {
-  displayValue = displayValue.slice(0, -1);
-  display.value = displayValue;
+  currentValue = currentScreen.textContent.slice(0, -1);
+  currentScreen.textContent = currentValue;
+}
+
+// Helper function to check if a number is a whole number
+function isWholeNumber(number) {
+  return Number.isInteger(number);
 }
 
 document.addEventListener("keydown", handleKeyPress);
@@ -103,15 +148,15 @@ function handleKeyPress(event) {
   const key = event.key;
 
   if (key >= "0" && key <= "9") {
-    appendToDisplay(key);
-  } else if (key === "." && displayValue.indexOf(".") === -1) {
-    appendToDisplay(key);
+    handleNumber(key);
+  } else if (key === "." && currentValue.indexOf(".") === -1) {
+    addDecimal();
   } else if (key === "+" || key === "-" || key === "*" || key === "/") {
-    appendToDisplay(key);
+    handleOperator(key);
   } else if (key === "Enter") {
     operate();
   } else if (key === "Delete") {
-    clearDisplay();
+    clearScreen();
   } else if (key === "Backspace") {
     deleteLastEntry();
   }
